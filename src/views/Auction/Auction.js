f3601f654783ccf18d99b792a7ba65352ee4523b
solids8n <solids8n@protonmail.com>
2022-01-06 22:34:28 +0300

    create: auction page

---
+++ src/views/Auction/Auction.js
@@ -0,0 +1,16 @@
+import { Paper, Zoom } from "@material-ui/core";
+import { useState } from "react";
+import "./auction.scss";
+
+const Auction = () => {
+  const [zoomed, setZoomed] = useState(false);
+  return (
+    <div id="stake-view">
+      <Zoom in={true} onEntered={() => setZoomed(true)}>
+        <Paper className={`ohm-card`}>Auction Page</Paper>
+      </Zoom>
+    </div>
+  );
+};
+
+export default Auction;
