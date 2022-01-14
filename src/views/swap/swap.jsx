50792db48c080ae0664bb1dd09707bdece87c52f
drwarlock <jessebhurt@gmail.com>
2022-01-14 18:49:54 -0500

    update to use ohm tokens

--- src/views/swap/swap.jsx
+++ src/views/swap/swap.jsx
@@ -1,11 +1,34 @@
 import { Box, Grid, Paper, Typography, Zoom, SvgIcon } from "@material-ui/core";
 import "./swap.scss";
 
+// for wallet access see const in swap function
+import { useWeb3Context } from "src/hooks/web3Context";
+
+// maintain state for some settings
+import { useCallback, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { Skeleton } from "@material-ui/lab";
+
 // TODO: 1 Implementation
+// add getimage to ./helpers/index.tsx
 //const aPHMImg = getaPHMTokenImage("");
 //const fPHMImg = getfPHMTokenImage("");
 
 function Swap() {
+  const { provider, address, connected, connect, chainID } = useWeb3Context();
+  const [zoomed, setZoomed] = useState(false);
+  const [view, setView] = useState(0);
+  const [quantity, setQuantity] = useState("");
+  const isAppLoading = useSelector(state => state.app.loading);
+
+  const ohmBalance = useSelector(state => {
+    return state.account.balances && state.account.balances.ohm;
+  });
+  const sohmBalance = useSelector(state => {
+    return state.account.balances && state.account.balances.sohm;
+  });
+
   return (
     <div id="swap-view">
       <Zoom in={true}>
@@ -30,15 +53,15 @@ function Swap() {
                   <Grid item xs={12} sm={4} md={4} lg={4}>
                     <div className="swap-balance">
                       <Typography variant="h5">BALANCE</Typography>
-                      <Typography variant="h4">aphm balance 3333.33</Typography>
-                      <Typography variant="h4">aphm unlocked 333.33</Typography>
+                      <Typography variant="h4">{isAppLoading ? <Skeleton width="80px" /> : <>{trim(ohmBalance, 4)} OHM</>}</Typography>
+                      <Typography variant="h4">{isAppLoading ? <Skeleton width="80px" /> : <>{trim(sohmBalance, 4)} sOHM</>}</Typography>
                     </div>
                   </Grid>
                   <Grid item xs={12} sm={4} md={4} lg={4}>
                     <div className="swap-unlocked">
                       <Typography variant="h5">UNLOCKED</Typography>
-                      <Typography variant="h4">aphm unlocked 333.33</Typography>
-                      <Typography variant="h4">fphm unlocked 33.33</Typography>
+                      <Typography variant="h4"></Typography>
+                      <Typography variant="h4"></Typography>
                     </div>
                   </Grid>
                   <Grid item xs={12} sm={4} md={4} lg={4}>
