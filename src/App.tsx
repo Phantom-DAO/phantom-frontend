ebf72cf213a6998356349d6e3bb5aba985486549
Hossam Saraya <dev@deadlock.sh>
2022-01-14 02:05:08 +0200

    Remove 33PoolTogether leftovers

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ import { loadAppDetails } from "./slices/AppSlice";
 import { loadAccountDetails, calculateUserBondDetails } from "./slices/AccountSlice";
 import { info } from "./slices/MessagesSlice";
 
-import { Stake, ChooseBond, Bond, Wrap, TreasuryDashboard, PoolTogether } from "./views";
+import { Stake, ChooseBond, Bond, Wrap, TreasuryDashboard } from "./views";
 import Sidebar from "./components/Sidebar/Sidebar.jsx";
 import TopBar from "./components/TopBar/TopBar.jsx";
 import NavDrawer from "./components/Sidebar/NavDrawer.jsx";
@@ -220,10 +220,6 @@ function App() {
 
             <Route path="/wrap">
               <Wrap />
-            </Route>
-
-            <Route path="/33-together">
-              <PoolTogether />
             </Route>
 
             <Route path="/bonds">
