1f71dbe850273e33acf93d3646cd698a97b0a7fb
drwarlock <jessebhurt@gmail.com>
2022-01-17 15:05:06 -0500

    update swap

--- src/views/swap/swap.jsx
+++ src/views/swap/swap.jsx
@@ -34,7 +34,7 @@ function Swap() {
     <div id="swap-view">
       <Zoom in={true}>
         <Paper className={`swap-card`}>
-          <Grid container direction="column">
+          <Grid container direction="column" spacing={7} rowspacing={1}>
             <Grid item>
               <Box className="swap-header">
                 <Typography variant="h3">Swap tokens</Typography>
@@ -43,7 +43,7 @@ function Swap() {
             </Grid>
             <Grid item>
               <div className="swap">
-                <Grid container alignItems="flex-end">
+                <Grid container spacing={2} alignItems="flex-end">
                   <Grid item xs={12} sm={4} md={4} lg={4}>
                     <div className="swap-tokens">
                       <Typography variant="h5">SWAP</Typography>
