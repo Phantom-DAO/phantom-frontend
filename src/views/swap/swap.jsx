55eea0abf9b660529249d142e4d988b151f9bdde
drwarlock <jessebhurt@gmail.com>
2022-01-17 18:07:07 -0500

    update swap grid

--- src/views/swap/swap.jsx
+++ src/views/swap/swap.jsx
@@ -35,7 +35,7 @@ function Swap() {
       <Zoom in={true}>
         <Paper className={`swap-card`}>
           <Grid container direction="column" spacing={7}>
-            <Grid item xs={12} sm={12} md={8} lg={4}>
+            <Grid item>
               <Box className="swap-header">
                 <Typography variant="h3">Swap tokens</Typography>
                 <Typography variant="h5">Swap your aPHM or fPHM tokens</Typography>
@@ -44,14 +44,14 @@ function Swap() {
             <Grid item>
               <div className="swap">
                 <Grid container spacing={1} alignItems="flex-end">
-                  <Grid item xs={6}>
+                  <Grid item xs={12}>
                     <div className="swap-tokens">
                       <Typography variant="h5">SWAP</Typography>
                       <Typography variant="h4">image aPHM to PHM</Typography>
                       <Typography variant="h4">image fPHM to PHM</Typography>
                     </div>
                   </Grid>
-                  <Grid item xs={2}>
+                  <Grid item xs={4}>
                     <div className="swap-balance">
                       <Typography variant="h5">BALANCE</Typography>
                       <Typography variant="h4">
@@ -62,7 +62,7 @@ function Swap() {
                       </Typography>
                     </div>
                   </Grid>
-                  <Grid item xs={2}>
+                  <Grid item xs={4}>
                     <div className="swap-unlocked">
                       <Typography variant="h5">UNLOCKED</Typography>
                       <Typography variant="h4">
@@ -73,7 +73,7 @@ function Swap() {
                       </Typography>
                     </div>
                   </Grid>
-                  <Grid item xs={2}>
+                  <Grid item xs={4}>
                     <div className="swap-button">
                       <Typography variant="h4">button</Typography>
                       <Typography variant="h4">button</Typography>
