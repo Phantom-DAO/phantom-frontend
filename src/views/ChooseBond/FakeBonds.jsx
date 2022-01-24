cf1773531115a5a253aec31e38823de9e0fb0312
r3l0cd <r3l0c@protonmail.com>
2022-01-23 21:52:51 -0500

    Updated ChooseBond views to match UI design

---
+++ src/views/ChooseBond/FakeBonds.jsx
@@ -0,0 +1,47 @@
+import { NetworkID } from "src/lib/Bond";
+
+import { ReactComponent as FraxImg } from "src/assets/tokens/FRAX.svg";
+import { ReactComponent as OhmEthImg } from "src/assets/tokens/OHM-WETH.svg";
+import { ReactComponent as wFTMImg } from "src/assets/tokens/wFTM.svg";
+
+export const frax = {
+  name: "frax",
+  displayName: "FRAX",
+  bondToken: "FRAX",
+  bondPrice: 114.584,
+  bondDiscount: 0.2,
+  purchased: 500,
+  pendingPayout: 0.442,
+  interestDue: 1.558,
+  bondMaturationBlock: 15000,
+  isAvailable: { [NetworkID.Mainnet]: true, [NetworkID.Testnet]: true },
+  bondIconSvg: FraxImg,
+};
+
+export const ftm = {
+  name: "ftm",
+  displayName: "wFTM",
+  bondToken: "wFTM",
+  bondPrice: 137.5,
+  bondDiscount: 0.04,
+  purchased: 0,
+  isAvailable: { [NetworkID.Mainnet]: false, [NetworkID.Testnet]: false },
+  bondIconSvg: wFTMImg,
+};
+
+export const phm_frax = {
+  name: "phm_frax_lp",
+  displayName: "PHM-FRAX LP",
+  bondToken: "FRAX",
+  bondPrice: 130.3393,
+  bondDiscount: 0.09,
+  purchased: 0,
+  isAvailable: { [NetworkID.Mainnet]: true, [NetworkID.Testnet]: true },
+  bondIconSvg: OhmEthImg,
+  lpUrl: "#",
+  isLP: true,
+};
+
+export const fakeBonds = [frax, ftm, phm_frax];
+
+export default fakeBonds;
